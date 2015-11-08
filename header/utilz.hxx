3672ac9535c31bf7d5eaee8e74f9ae8847670d23
Dan Silver <dannysilver3@gmail.com>
2015-11-07 19:53:25 -0600

    padding images

--- header/utilz.hxx
+++ header/utilz.hxx
@@ -6,9 +6,29 @@
 #include <string>
 #include <iostream>
 #include <sstream>
-#include <math.h>       /* log10 */
+#include <math.h>
+
+
+cimg_library::CImg<> addPaddingToImage(cimg_library::CImg<> image, int pixels) {
+    cimg_library::CImg<> padded_image(image.width() + pixels, image.height() + pixels, 1, image.spectrum());
+
+
+    for (int channel = 0; channel < image.spectrum(); channel++) {
+        for (int r = pixels ; r < image.height() + pixels; r++) {
+            for (int c= pixels ; c < image.width() + pixels; c++) {
+                padded_image(r, c, 0, channel) = (int) image(r, c, 0, channel);
+            }
+        }
+    }
+
+    return padded_image;
+}
 
 void detect_edges(cimg_library::CImg<> input_image) {
+    /** add padding to input image **/
+
+    cimg_library::CImg<> paddedImage = addPaddingToImage(input_image, 2);
+    paddedImage.display("padded image");
 
     /** define sobel filters **/
     int G_x[3][3] = {
@@ -31,9 +51,11 @@ void detect_edges(cimg_library::CImg<> input_image) {
     int channel = 0;
     int value;
 
-    for (int r = 0 ; r < input_image.height(); r++) {
-        for (int c= 0 ; c < input_image.width(); c++) {
-            value = (int) input_image(c, r, 0, channel);
+    for (int channel = 0; channel < input_image.spectrum(); channel++) {
+        for (int r = 0 ; r < input_image.height(); r++) {
+            for (int c= 0 ; c < input_image.width(); c++) {
+                value = (int) input_image(r, c, 0, channel);
+            }
         }
     }
 
